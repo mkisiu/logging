20032604afef92f8fc19007a4ec3aef0297ab3f4
mkisiu <maciek.kisiel@gmail.com>
2025-05-05 16:36:13 +0200

    fixed return value in syslog_linux.go - shoule be a reference instead of value.

--- syslog_linux.go
+++ syslog_linux.go
@@ -6,6 +6,6 @@ import (
 	"log/syslog"
 )
 
-func getSyslogWriter() (syslog.Writer, error) {
+func getSyslogWriter() (*syslog.Writer, error) {
 	return syslog.New(syslog.LOG_INFO|syslog.LOG_LOCAL7, "")
 }
